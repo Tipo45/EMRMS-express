4ae3e24cfc64aabd533f4b83b108be209fee872b
Azebi-Alex Steven .T. <tipo4542@gmail.com>
2025-05-07 21:09:29 +0100

    last commit

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post("/send-otp", async (req, res) => {
 
   try {
     const response = await resend.emails.send({
-        from: "emrms <emrmsproject2025@emrms.online>",
+        from: "EMRMS <emrmsproject2025@emrms.online>",
         to: [email],
         subject: subject,
         html: message,
